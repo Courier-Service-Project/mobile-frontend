732661ccaca15db8ad6e9ae42281922f7a04098a
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2024-06-16 19:27:24 +0530

    new home

--- custom_modules/screens/homeScreen.js
+++ custom_modules/screens/homeScreen.js
@@ -35,7 +35,7 @@ const HomeScreen = () => {
     const branchLocation = await AsyncStorage.getItem('branchLocation');
     try {
       const result = await axios.get(
-        `http://192.167.87.9:9000/api/mobile/orders/getOrderStateCount/${user_id}/${branchLocation}`,
+        `http://10.10.27.131:9000/api/mobile/orders/getOrderStateCount/${user_id}/${branchLocation}`,
       );
       setOrderStatusCount(result.data.message);
       console.log(result.data.message[4][0].DiliveryProvince);
